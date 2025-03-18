a00abdc8838f744c34cfb564fcf050c1acf1479f
LucHeart <luc@luc.cat>
2025-03-18 09:40:13 +0100

    Use hex strings instead of base64

--- DesktopRepositoryServer/Models/CreateModuleVersionRequest.cs
+++ DesktopRepositoryServer/Models/CreateModuleVersionRequest.cs
@@ -1,8 +1,12 @@
-﻿namespace OpenShock.Desktop.RepositoryServer.Models;
+﻿using System.Text.Json.Serialization;
+using OpenShock.Desktop.RepositoryServer.Utils;
+
+namespace OpenShock.Desktop.RepositoryServer.Models;
 
 public sealed class CreateModuleVersionRequest
 {
     public required Uri ZipUrl { get; init; }
+    [JsonConverter(typeof(ByteArrayHexConverter))]
     public required byte[] Sha256Hash { get; init; }
     public Uri? ChangelogUrl { get; init; } = null;
     public Uri? ReleaseUrl { get; init; } = null;
---
+++ DesktopRepositoryServer/Utils/ByteArrayHexConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace OpenShock.Desktop.RepositoryServer.Utils;
+
+public sealed class ByteArrayHexConverter : JsonConverter<byte[]>
+{
+    public override byte[] Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.String)
+            throw new JsonException($"Expected string, but got {reader.TokenType}");
+        
+        return Convert.FromHexString(reader.GetString()!);
+    }
+
+    public override void Write(Utf8JsonWriter writer, byte[] value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(Convert.ToHexString(value));
+    }
+}
